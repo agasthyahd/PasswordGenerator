ee2361fd57c1a10b8f8531b338dd4b14d07dbf5b
Agasthya HD <agasthyahd@gmail.com>
2023-06-17 00:57:00 -0400

    password generator

---
+++ password-gen.py
@@ -0,0 +1,29 @@
+import pandas
+import string
+import random
+
+
+if __name__ == '__main__':
+
+    s = string.ascii_lowercase
+    # print(s)
+    s2 = string.ascii_uppercase
+
+    s3 = string.digits
+    # print(s3)
+
+    pass_len = int(input('Enter the length of your password.\n'))
+    pass_list = []
+    pass_list.extend(list(s)*10)
+    pass_list.extend(list(s2)*10)
+    pass_list.extend(list(s3)*10)
+
+    random.shuffle(pass_list)
+    print(pass_list)
+
+
+    pass_generated = "".join(pass_list[0:(pass_len)])
+
+    printsent='Password generated!\nYour password is {}';
+    print(printsent.format(pass_generated))
+    print(len(pass_generated))
